607c43021fab64e0a02f74cfb741e2a1a54060ba
s2s2hyun <s2s2hyun0601@gmail.com>
2023-10-29 23:41:11 +0900

    fix optinal

--- src/components/Element/Tag.tsx
+++ src/components/Element/Tag.tsx
@@ -5,7 +5,7 @@ import React from "react";
 interface TagProps  {
   link?: string;
   name: string;
-  className?: any;
+  className?: string;
 }
 
 
@@ -15,7 +15,7 @@ const Tag = ({ link = "", name, ...props }: TagProps) => {
       href={link}
       className={cx(
         "inline-block py-3 px-10 bg-dark text-light rounded-full capitalize font-semibold border border-solid border-light hover:scale-105 transition-all ease duration-200",
-        props.className
+        props.className ?? ""
       )}>
       {name}
     </Link>
