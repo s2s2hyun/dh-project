7b939b388ebcad7ecc95ffe7f892411255562b50
s2s2hyun <s2s2hyun0601@gmail.com>
2023-10-30 22:02:39 +0900

    wait blog port

--- src/components/Blog/BlogLayoutOne.tsx
+++ src/components/Blog/BlogLayoutOne.tsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import Tag from "../Element/Tag";
+import Link from "next/link";
 
-const BlogLayoutOne = () => {
+const BlogLayoutOne = ({ blog }: any) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="inline-block overflow-hidden rounded-xl">
+      <div className="absolute top-0 left-0 bottom-0 right-0 h-full bg-gradient-to-b from-transparent from-0% to-dark rounded-3xl z-0" />
+      <Image
+        src={blog.image?.filePath.replace("../public", "")}
+        placeholder="blur"
+        blurDataURL={blog.image.blurhashDataUrl}
+        alt={blog.title}
+        width={blog.image.width}
+        height={blog.image.height}
+        fill
+        className="w-full h-full object-center object-cover rounded-3xl -z-10"
+      />
+      <div className="w-3/4 p-16 flex flex-col items-start justify-center z-0 text-light">
+        {/* <Link href={`/categories/${blog.tags[0]}`}>{blog.tags[0]}</Link> */}
+        <Tag link={`/categories/${blog.tags[0]}`} name={blog.tags[0]} />
+        <Link href={blog.url} className="mt-6">
+          <h1 className="font-bold capitalize text-4xl ">
+            <span
+              className="bg-gradient-to-r from-accent to-accent dark:from-accentDark/50 
+                dark:to-accentDark/50 bg-[length:0px_6px]
+                hover:bg-[length:100%_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 "
+            >
+              {blog.title}
+            </span>
+          </h1>
+        </Link>
 
-export default BlogLayoutOne
+        <p className="inline-block mt-4 text-xl font-in">{blog.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default BlogLayoutOne;
