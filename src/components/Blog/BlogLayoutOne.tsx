65f0e0ac132983a9b5426f9c18a53b2cb4b87437
s2s2hyun <s2s2hyun0601@gmail.com>
2023-11-01 00:00:01 +0900

    fix text

--- src/components/Blog/BlogLayoutOne.tsx
+++ src/components/Blog/BlogLayoutOne.tsx
@@ -27,7 +27,7 @@ const BlogLayoutOne = ({ blog }: any) => {
               className="bg-gradient-to-r from-accent to-accent bg-[length:0px_6px]
                 hover:bg-[length:100%_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 "
             >
-              {blog.title}
+              {blog.title}s
             </span>
           </h2>
         </Link>
