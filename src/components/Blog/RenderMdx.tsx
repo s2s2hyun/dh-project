dfb63f55135f3bff235dede67f5aeae80cdb9d37
s2s2hyun <s2s2hyun0601@gmail.com>
2023-11-27 17:27:59 +0900

    fix blog css -1 조금더 추가 예정

--- src/components/Blog/RenderMdx.tsx
+++ src/components/Blog/RenderMdx.tsx
@@ -14,17 +14,18 @@ const RenderMdx = ({ blog }: any) => {
     <div
       className=" sm-prose-base  prose col-span-12 max-w-max font-in dark:prose-invert
      md:prose-lg
-      first-letter:text-3xl 
-      prose-blockquote:rounded-r-lg prose-blockquote:border-accent
-       prose-blockquote:bg-accent/20
-       
+     first-letter:text-3xl
+      prose-blockquote:rounded-r-lg 
+      prose-blockquote:border-accent prose-blockquote:bg-accent/20
        prose-blockquote:p-2
+       
        prose-blockquote:px-6
        prose-blockquote:not-italic
        prose-li:marker:text-accent
-       dark:prose-blockquote:border-accentDark dark:prose-blockquote:bg-accentDark/20
-       dark:prose-li:marker:text-accentDark sm:col-span-8
-       sm:first-letter:text-5xl
+       dark:prose-blockquote:border-accentDark
+       dark:prose-blockquote:bg-accentDark/20 dark:prose-li:marker:text-accentDark
+       sm:col-span-12 sm:first-letter:text-5xl
+       lg:col-span-8
        "
     >
       <MDXContent components={mdxComponents} />
