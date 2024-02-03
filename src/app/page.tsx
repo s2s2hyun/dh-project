83ceeb4db6f6a032f17bf562e096549556bc4a9a
s2s2hyun <s2s2hyun0601@gmail.com>
2024-02-04 00:31:23 +0900

    /   page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,43 +4,43 @@ import { allBlogs } from 'contentlayer/generated'
 import FeaturedPost from '../components/Home/FeaturedPost'
 import RecentPost from '../components/Home/RecentPost'
 import { BlogPost } from '@/types/BlogPost'
-import { Metadata } from 'next'
+// import { Metadata } from 'next'
 import SiteMetaData from '../utils/siteMetaData'
 
-export const metadata: Metadata = {
-  metadataBase: new URL(SiteMetaData.siteUrl),
-  title: {
-    template: `%s | ${SiteMetaData.title}`,
-    default: SiteMetaData.title,
-  },
-  description: SiteMetaData.description,
-  openGraph: {
-    title: SiteMetaData.title,
-    description: SiteMetaData.description,
-    url: SiteMetaData.siteUrl,
-    siteName: SiteMetaData.title,
-    images: [SiteMetaData.socialBanner],
-    locale: 'ko-KR',
-    type: 'website',
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: false,
-      noimageindex: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: SiteMetaData.title,
-    images: [SiteMetaData.socialBanner],
-  },
-}
+// export const metadata: Metadata = {
+//   metadataBase: new URL(SiteMetaData.siteUrl),
+//   title: {
+//     template: `%s | ${SiteMetaData.title}`,
+//     default: SiteMetaData.title,
+//   },
+//   description: SiteMetaData.description,
+//   openGraph: {
+//     title: SiteMetaData.title,
+//     description: SiteMetaData.description,
+//     url: SiteMetaData.siteUrl,
+//     siteName: SiteMetaData.title,
+//     images: [SiteMetaData.socialBanner],
+//     locale: 'ko-KR',
+//     type: 'website',
+//   },
+//   robots: {
+//     index: true,
+//     follow: true,
+//     googleBot: {
+//       index: true,
+//       follow: false,
+//       noimageindex: true,
+//       'max-video-preview': -1,
+//       'max-image-preview': 'large',
+//       'max-snippet': -1,
+//     },
+//   },
+//   twitter: {
+//     card: 'summary_large_image',
+//     title: SiteMetaData.title,
+//     images: [SiteMetaData.socialBanner],
+//   },
+// }
 
 export default function Home() {
   return (
