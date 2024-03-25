417efdbf6ae2bd4a3c70f112354748bde3ef6aca
s2s2hyun <s2s2hyun0601@gmail.com>
2024-03-25 20:27:11 +0900

    bfcache 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,41 @@
+'use client'
+
 import Image from 'next/image'
 import HomeCoverSection from '../components/Home/HomeCoverSection'
 import { allBlogs } from 'contentlayer/generated'
 import FeaturedPost from '../components/Home/FeaturedPost'
 import RecentPost from '../components/Home/RecentPost'
 import { BlogPost } from '@/types/BlogPost'
-// import { Metadata } from 'next'
 import SiteMetaData from '../utils/siteMetaData'
+import { useEffect } from 'react'
 
-// export const metadata: Metadata = {
-//   metadataBase: new URL(SiteMetaData.siteUrl),
-//   title: {
-//     template: `%s | ${SiteMetaData.title}`,
-//     default: SiteMetaData.title,
-//   },
-//   description: SiteMetaData.description,
-//   openGraph: {
-//     title: SiteMetaData.title,
-//     description: SiteMetaData.description,
-//     url: SiteMetaData.siteUrl,
-//     siteName: SiteMetaData.title,
-//     images: [SiteMetaData.socialBanner],
-//     locale: 'ko-KR',
-//     type: 'website',
-//   },
-//   robots: {
-//     index: true,
-//     follow: true,
-//     googleBot: {
-//       index: true,
-//       follow: false,
-//       noimageindex: true,
-//       'max-video-preview': -1,
-//       'max-image-preview': 'large',
-//       'max-snippet': -1,
-//     },
-//   },
-//   twitter: {
-//     card: 'summary_large_image',
-//     title: SiteMetaData.title,
-//     images: [SiteMetaData.socialBanner],
-//   },
-// }
+interface PageTransitionEvent extends Event {
+  readonly persisted: boolean
+}
 
 export default function Home() {
+  // bfcache 테스트
+
+  useEffect(() => {
+    const handlePageShow = (event: PageTransitionEvent) => {
+      if (event.persisted) {
+        // 여기서 페이지가 bfcache로부터 복원될 때 필요한 상태 업데이트 수행
+        console.log('Page was restored from bfcache.')
+        // 예: setIsLoading(false);
+      }
+    }
+
+    window.addEventListener('pageshow', handlePageShow)
+
+    // 컴포넌트 언마운트 시 이벤트 리스너 제거
+    return () => {
+      window.removeEventListener('pageshow', handlePageShow)
+    }
+  }, [])
+
   return (
     <main className="flex flex-col items-center justify-center ">
       <HomeCoverSection blogs={allBlogs as BlogPost[]} />
-      {/* <FeaturedPost blogs={allBlogs as BlogPost[]} /> */}
       <RecentPost blogs={allBlogs as BlogPost[]} />
     </main>
   )
